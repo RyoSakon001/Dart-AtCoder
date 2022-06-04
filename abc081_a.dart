ffae5c97df88a5858d39e0f53e34982e48c7e4b8
sakon-ziwave <ryo.development0320@gmail.com>
2022-06-04 12:41:29 +0900

    Beginners Colection

---
+++ abc081_a.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+
+  String? str_tmp = stdin.readLineSync();
+  final String s = str_tmp!;
+
+  if (s.contains('1')) print('1を含む');
+  exit;
+
+  var answer = '';
+  print(answer);
+}
+
+
+
+
+
+
+
+
